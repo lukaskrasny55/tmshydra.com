3522e627a1da264c11299cb660fb82304fd69cc9
lukaskrasny55 <lukaskrasny55@gmail.com>
2026-05-06 08:22:30 +0200

    Refactor GoogleAds component for static ads ID

--- components/GoogleAds.tsx
+++ components/GoogleAds.tsx
@@ -1,61 +1,30 @@
 import React, { useEffect } from 'react';
-import { supabase } from '../lib/supabase';
 
 export const GoogleAds: React.FC = () => {
   useEffect(() => {
-    const fetchSettings = async () => {
-      try {
-        const { data, error } = await supabase.from('settings').select('*').single();
-        if (!error && data && data.google_ads_id) {
-          // GTM Script
-          if (data.google_tag_manager_id) {
-            const gtmScript = document.createElement('script');
-            gtmScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${data.google_tag_manager_id}');`;
-            document.head.appendChild(gtmScript);
-          }
+    const adsId = 'AW-XXXXXXXXXX';
 
-          // Google Ads Script
-          const adsScript = document.createElement('script');
-          adsScript.async = true;
-          adsScript.src = `https://www.googletagmanager.com/gtag/js?id=${data.google_ads_id}`;
-          document.head.appendChild(adsScript);
+    const adsScript = document.createElement('script');
+    adsScript.async = true;
+    adsScript.src = `https://www.googletagmanager.com/gtag/js?id=${adsId}`;
+    document.head.appendChild(adsScript);
 
-          const inlineScript = document.createElement('script');
-          inlineScript.innerHTML = `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${data.google_ads_id}');
-          `;
-          document.head.appendChild(inlineScript);
-        }
-      } catch (err) {
-        console.error('Error in GoogleAds fetchSettings:', err);
-      }
-    };
+    const inlineScript = document.createElement('script');
+    inlineScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${adsId}');
+    `;
 
-    fetchSettings();
+    document.head.appendChild(inlineScript);
   }, []);
 
   return null;
 };
 
-export const trackConversion = async (eventName: 'form' | 'booking') => {
-  try {
-    const { data, error } = await supabase.from('settings').select('*').single();
-    if (!error && data) {
-      const eventId = eventName === 'form' ? data.conversion_event_form : data.conversion_event_booking;
-      if (eventId && (window as any).gtag) {
-        (window as any).gtag('event', 'conversion', {
-          'send_to': `${data.google_ads_id}/${eventId}`,
-        });
-      }
-    }
-  } catch (err) {
-    console.error('Error in trackConversion:', err);
+export const trackConversion = (eventName: 'form' | 'booking') => {
+  if ((window as any).gtag) {
+    (window as any).gtag('event', 'conversion');
   }
 };
